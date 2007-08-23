749ba15f5e847f112d4dd8523db68de7b6fee3b4
Jochen Topf <jochen@topf.org>
2007-08-23 14:25:11 +0000

    Paths for compiling on Mac OS X

--- src/extconf.rb
+++ src/extconf.rb
@@ -1,4 +1,8 @@
-#should add an option for the proj.lib / proj.so + headers somewhere...
 require 'mkmf'
+
+# help with compiling on Mac OS X
+$CFLAGS += " -I/sw/include"
+$LDFLAGS += " -L/sw/lib"
+
 have_library 'proj', 'pj_init'
 create_makefile 'projrb'
